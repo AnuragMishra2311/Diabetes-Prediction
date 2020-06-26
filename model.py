9272217d118799c8ca3a91787faedde8c528851c
AnuragMishra2311 <55075448+AnuragMishra2311@users.noreply.github.com>
2020-06-26 23:37:46 +0530

    Removed comments and extra libraries

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
+#importing libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
-from sklearn.model_selection import RandomizedSearchCV,StratifiedKFold
 import pickle
 
 #Reading File
@@ -16,35 +15,20 @@ def replacing_0_with_NAN():
 
 replacing_0_with_NAN()
 
-#Null Values
+#Handling Null Values
 df.Glucose.fillna(df.Glucose.mean(),inplace=True)
 df.BloodPressure.fillna(df.BloodPressure.mean(),inplace=True)
 df.SkinThickness.fillna(df.SkinThickness.median(),inplace=True)
 df.Insulin.fillna(df.Insulin.mean(),inplace=True)
 df.BMI.fillna(df.BMI.median(),inplace=True)
 
-#Model
+
 x=df.iloc[:,:-1].values
 y=df.iloc[:,8].values
 
 # Splitting
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
-
-
-
-'''Hyperparameter Tuning
-params={
-             "learning_rate"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30 ] ,
-             "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
-             "min_child_weight" : [ 1, 3, 5, 7 ],
-             "gamma"            : [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
-             "colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ]
-    }
-
-classifier=XGBClassifier()
-rs=RandomizedSearchCV(classifier,param_distributions=params,n_iter=15,cv=StratifiedKFold(n_splits=10))
-rs.fit(X_train,y_train)'''
 
 classifier=XGBClassifier(min_child_weight=1,max_depth=12,learning_rate=0.25,gamma= 0.4,colsample_bytree=0.5)
 
